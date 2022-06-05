8ab0b57be2cb77f067b46e1dba2341e89c918f9d
Kimberly Marroquin <marroquin.kim3@gmail.com>
2022-06-05 19:44:10 -0400

    changed port number

--- bab-frontend/src/Variables.js
+++ bab-frontend/src/Variables.js
@@ -1,4 +1,4 @@
 export const variables = {
-    API_URL: "http://localhost:5001/api/",
-    PHOTO_URL: "http://localhost:5001/Photos/"
+    API_URL: "http://localhost:44336/api/",
+    PHOTO_URL: "http://localhost:44336/Photos/"
 }
