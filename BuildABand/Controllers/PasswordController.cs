8ee96c6b33f9967468c0ebb9b46534e2cef729b8
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-09 04:12:29 +0100

    Added controller for getting password hash

---
+++ BuildABand/Controllers/PasswordController.cs
@@ -0,0 +1,32 @@
+﻿using BuildABand.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Controllers
+{
+    // <summary>
+    /// This class serves as the controller,
+    /// for hashing password
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public PasswordController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("{password}")]
+        public JsonResult GetSha256Hash(string password)
+        {
+            return new JsonResult( PasswordHash.GetSha256Hash(password));
+        }
+    }
+}
