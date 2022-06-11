8ca652e510e9d0e63a19dff0707317249cdb9d99
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-11 20:11:30 +0100

    Added code  for user class

---
+++ BuildABand/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Models
+{
+    public class User
+    {
+        public int AccountID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Active { get; set; }
+        public int MusicianID { get; set; }
+        public string Fname {get; set;}
+        public string Lname { get; set; }
+        public DateTime DateOfbirth { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+        public string Instrument { get; set; }
+        public string Sex { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string StateCode { get; set; }
+        public string AvaterFilename { get; set; }
+
+    }
+}
