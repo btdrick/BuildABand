d0437a66eef8d2e9ba910c54b4e4095a81160646
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-14 08:10:17 +0100

    Added code for StatesController to get states

--- BuildABand/DAL/StatesDAL.cs
+++ BuildABand/DAL/StatesDAL.cs
@@ -8,19 +8,31 @@ using System.Threading.Tasks;
 
 namespace BuildABand.DAL
 {
+    /// <summary>
+    /// This class serves as the Data Access Layer
+    /// for DB States table.
+    /// </summary>
     public class StatesDAL
     {
         private IConfiguration _configuration;
 
+        /// <summary>
+        /// Constructor to initialize configuration variable
+        /// </summary>
+        /// <param name="configuration"></param>
         public StatesDAL(IConfiguration configuration)
         {
             this._configuration = configuration;
         }
 
+        /// <summary>
+        /// Get list of all States.
+        /// </summary>
+        /// <returns></returns>
         public List<State> GetStates()
         {
             List<State> states = new List<State>();
-            string selectStatement = "Select * from State";
+            string selectStatement = "Select * from States";
 
             using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("BuildABandAppCon")))
             {
