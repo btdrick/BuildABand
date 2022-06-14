d41f43d0b1db8ced63dcf7979bb8bda696318b13
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-14 07:46:18 +0100

    Added State Class and StatesDAL with GETStates method

---
+++ BuildABand/DAL/StatesDAL.cs
@@ -0,0 +1,46 @@
+﻿using BuildABand.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.DAL
+{
+    public class StatesDAL
+    {
+        private IConfiguration _configuration;
+
+        public StatesDAL(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public List<State> GetStates()
+        {
+            List<State> states = new List<State>();
+            string selectStatement = "Select * from State";
+
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("BuildABandAppCon")))
+            {
+                connection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            State state = new State();
+                            state.StateCode = reader["StateCode"].ToString();
+                            state.StateName = reader["StateName"].ToString();
+                            states.Add(state);
+                        }
+                    }
+                }
+
+            }
+            return states;
+        }
+    }
+}
---
+++ BuildABand/Models/State.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Models
+{
+    /// <summary>
+    /// This class models a state object.
+    /// </summary>
+    public class State
+    {
+        /// <summary>
+        /// State attributes.
+        /// </summary>
+        public string StateCode { get; set; }
+        public string StateName { get; set; }
+    }
+}
