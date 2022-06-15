b83d874b9c92c3c24192a6a8127e37148cbd1feb
brandond-westga <bd00051@my.westga.edu>
2022-06-15 18:10:38 -0400

    Comments can have replies. NOTE: nested comments are currently not allowed for replies

--- bab-frontend/src/components/comments/Comments.js
+++ bab-frontend/src/components/comments/Comments.js
@@ -3,13 +3,24 @@ import {variables} from '../../Variables.js';
 import Comment from './Comment';
 
 /* User will only be able to edit, delete comments linked to their UserID */
-const Comments = ({currentUserID}) => {
+const Comments = ({ currentUserID }) => {
+    
     /* All comments from backend */
     const [backendComments, setBackendComments] = useState([]);
+    
     /* Parent comments, not replies */
     const rootComments = backendComments   
     .filter(
         (backendComment) => backendComment.ParentID === null);
+    
+        /* Replies to comments */
+    const getReplies = (commentID) =>
+    backendComments
+        .filter((backendComment) => backendComment.ParentID === commentID)
+        .sort(
+        (a, b) =>
+            new Date(a.CreatedTime).getTime() - new Date(b.CreatedTime).getTime()
+        );
 
     /* Sets backend comments */
      useEffect(() => {
@@ -28,8 +39,12 @@ const Comments = ({currentUserID}) => {
         <div className="comments">
             <h3 className="comments-title">Comments</h3>
             <div className="comments-container">
+                {/* Map comments and their replies by parent */}
                 {rootComments.map(rootComment => (
-                    <Comment key={rootComment.CommentID} comment={rootComment} />
+                    <Comment 
+                    key={rootComment.CommentID} 
+                    comment={rootComment} 
+                    replies={getReplies(rootComment.CommentID)} />
                 ))}
             </div>
         </div>
