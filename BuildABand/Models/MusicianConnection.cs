b1c3302139a21651115fd2d59267c442c6cc5fa1
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-17 00:44:46 +0100

    Added MusicianConnection class

---
+++ BuildABand/Models/MusicianConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Models
+{
+    /// <summary>
+    /// This class models a state object.
+    /// </summary>
+    public class MusicianConnection
+    {
+        /// <summary>
+        /// Musician connection attributes.
+        /// </summary>
+        public int ConnectionID { get; set; }
+        public int InitiatorID { get; set; }
+        public int FollowerID { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public bool Connected { get; set; }
+
+    }
+}
