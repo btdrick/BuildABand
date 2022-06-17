e79ac473f4ac0bb0061131f1b17a530e458acd6b
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-18 00:18:59 +0100

    Create connections component using componentDIdMount method to fetch connection by musicianID

--- bab-frontend/src/Connections.jsx
+++ bab-frontend/src/Connections.jsx
@@ -5,5 +5,23 @@ class Connections extends Component{
     
     constructor(){
         super();
+        this.state = {
+            connections: []
+        };
     }
+
+    //Get connections
+    componentDidMount(){
+        fetch(variables.API_URL + "musicianconnections/" + this.props.musicianID)
+            .then(res => res.json())
+            .then(result => {
+                this.setState({
+                    connections:result
+                });
+            })
+    }
+
+
 }
+
+export default Connections;
