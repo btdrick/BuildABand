7bd99d249546a07fd401b4a617c69eff45597ab2
Chijioke Amaefuna <ca00077@my.westga.edu>
2022-06-20 13:54:08 +0100

    Added code for AddConnection component

--- bab-frontend/src/Profile.jsx
+++ bab-frontend/src/Profile.jsx
@@ -3,6 +3,7 @@ import { Post } from './Post';
 import {variables} from './Variables.js';
 import UserProfile from './components/UserProfile.js';
 import Navbar from './components/header/Navbar';
+import AddConnection from './components/connection/AddConnection.js'
 
 export class Profile extends Component {
     /* Constructor for the component. Tracks the posts shown on the profile and whether the page's loading status */
@@ -127,7 +128,9 @@ export class Profile extends Component {
                     <ul>
                         {this.state.posts.map((post, index) => 
                         <li key={index}><Post content={post.Content} /></li>)}
-                    </ul>                   
+                        <AddConnection followerID={this.state.MusicianID}/>    
+                    </ul>
+                               
                 </div>
             )
         }
