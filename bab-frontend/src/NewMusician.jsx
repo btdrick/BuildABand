6304cdbd0a339cc93f881d4f1b8c6b09f7ad5689
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-24 03:23:00 +0100

    Add Zipcode validation

--- bab-frontend/src/NewMusician.jsx
+++ bab-frontend/src/NewMusician.jsx
@@ -145,6 +145,15 @@ class NewMusician extends Component {
         errors["stateCode"] = "Please select your state.";
       }
 
+      if (typeof input["zipCode"] !== "undefined"){
+         const pattern = new RegExp(/(^\d{5}$)|(^\d{5}-\d{4}$)/);
+        if (!pattern.test(input["zipcode"])){
+          isValid = false;
+          errors["zipCode"] = "Please enter valid Zip Code.";
+        }
+      }
+     
+
 
       if (!input["dateOfBirth"]) {
         isValid = false;
@@ -152,8 +161,7 @@ class NewMusician extends Component {
       }
 
       if (typeof input["email"] !== "undefined") {
-          
-        var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
         if (!pattern.test(input["email"])) {
           isValid = false;
           errors["email"] = "Please enter valid email address.";
