8bad6d1356bfacbaef226c2b74cdce66a4402b20
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-28 23:55:37 +0100

    Add message component

---
+++ bab-frontend/src/components/message/Message.jsx
@@ -0,0 +1,14 @@
+import "./Message.css"
+
+export default function Message({own}){
+    return(
+        <div className={own? "message own" : "message"}>
+            <div className="messageTop">
+                <img className="messageImg" src="https://images.unsplash.com/photo-1656342696961-8716ada75c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80" alt="" />
+                 <p className="messageText"> Hi everyone this is real </p>
+            </div>
+           
+            <div className="messageBottom">1 hour ago</div>
+        </div>
+    )
+}
