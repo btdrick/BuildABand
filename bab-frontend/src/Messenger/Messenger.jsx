c708a17f93232201c933a7348758474a383bfd65
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-29 00:57:04 +0100

    Added Y-scroll and chattext box

--- bab-frontend/src/Messenger/Messenger.jsx
+++ bab-frontend/src/Messenger/Messenger.jsx
@@ -1,5 +1,7 @@
 import "./Messenger.css"
 import Navbar from "../components/header/Navbar"
+import Conversation from "../components/conversations/conversation"
+import Message from "../components/message/Message"
 
 export default function Messenger(){
     return(
@@ -9,10 +11,42 @@ export default function Messenger(){
             <div className="chatMenu">
                 <div className="chatMenuWrapper">
                     <input placeholder="Search for friends" className="chatMenuInput" />
+                     <Conversation />
+                     <Conversation />
+                     <Conversation />
+                     <Conversation />
                 </div>
             </div>
             <div className="chatBox">
-               <div className="chatBoxWrapper">Chat</div> 
+               <div className="chatBoxWrapper">
+                <div className="chatBoxTop">
+                    <Message />
+                    <Message own={true} />
+                    <Message />
+                    <Message />
+                    <Message own={true} />
+                    <Message />
+                    <Message />
+                    <Message />
+                    <Message own={true} />
+                    <Message />
+                    <Message />
+                    <Message own={true} />
+                    <Message />
+                    <Message />
+                    <Message />
+                    <Message own={true} />
+                    <Message />
+                    <Message />
+                    <Message own={true} />
+                    <Message />
+                    <Message />
+                </div>
+                <div className="chatBoxBottom">
+                    <textarea className="chatMessageInput" placeholder="Write something"></textarea>
+                    <button className="chatSubmitButton">Send</button>
+                </div>
+              </div>      
             </div>
             <div className="chatOnline">
                 <div className="chatOnlineWrapper">Online</div>
