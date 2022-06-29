4a0ad88ac0240d7412bcc62e1d15ab388304079a
Kimberly Marroquin <marroquin.kim3@gmail.com>
2022-06-29 19:28:20 -0400

    configured get audio api call

--- BuildABand/Controllers/AudioController.cs
+++ BuildABand/Controllers/AudioController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace BuildABand.Controllers
 {
@@ -10,6 +13,7 @@ namespace BuildABand.Controllers
     public class AudioController : ControllerBase
     {
         private readonly IConfiguration _configuration;
+        private readonly string connectionString;
 
         public AudioController(IConfiguration configuration)
         {
@@ -19,29 +23,51 @@ namespace BuildABand.Controllers
             // environment variable is created after the application is launched in a
             // console or with Visual Studio, the shell or application needs to be closed
             // and reloaded to take the environment variable into account.
-            string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
 
             _configuration = configuration;
         }
 
         //todo: put audio files in backend Audio folder
-        [HttpPost("blob")]
-        public void postBlob()
+        // used data annotations: https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations?view=net-6.0
+        [HttpPost]
+        [Consumes("application/octet-stream")]
+        public async Task postBlobAsync([FromQuery][Required][MinLength(3)] string filename)
         {
+            //var guid = Guid.NewGuid();
 
+            //await MusicDAL.addUserFileNameToAzureFileNameMapping(guid, userFileName);
+
+            //add to sql table
+            await uploadBlobAsync(filename, Request.Body);
         }
 
         //todo: upload blob to azure
-        public void uploadBlob()
+        public async Task uploadBlobAsync(string azureFileName, Stream content)
         {
-            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("audio");
+            BlobClient blobClient = containerClient.GetBlobClient(azureFileName);
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+            await blobClient.UploadAsync(content, overwrite: true);
         }
 
-        //todo: get blob from azure
-        [HttpGet("blob")]
-        public JsonResult getBlob()
+        [HttpGet("info")]
+        public JsonResult getFileInfo([FromQuery] int memberID)
         {
             return null;
+        }
+
+        [HttpGet("blob")]
+        public async Task<FileStreamResult> getBlob([FromQuery] string azureFileName)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("audio");
+            BlobClient blobClient = containerClient.GetBlobClient(azureFileName);
+
+            var result = await blobClient.DownloadStreamingAsync();
+            return new FileStreamResult(result.Value.Content, "application/octet-stream"); // assuming wave format
         }
     }
 }
