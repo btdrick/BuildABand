3469a856563c61787ff1751baa972aaed21855b5
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-30 01:31:38 +0100

    Added Conversation model class

---
+++ BuildABand/Models/Conversation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Models
+{
+    public class Conversation
+    {
+        public int ConversationID { get; set; }
+        public int MusicianID { get; set; }
+        public DateTime CreatedTime { get; set; }
+
+        public string Type { get; set; }
+    }
+}
