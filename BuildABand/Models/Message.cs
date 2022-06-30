edf05df7743a40f8e43c861fcd4e381b1102d63c
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-06-30 01:25:51 +0100

    Added Message model class

---
+++ BuildABand/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Models
+{
+    public class Message
+    {
+        public int MessageID  {get; set;}
+        public DateTime CreatedTime { get; set; }
+        public int SenderID { get; set; }
+        public string Text { get; set; }
+
+    }
+}
