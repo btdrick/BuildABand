41531ca0c0d51b29fb8f98e1a7b5df8b5b4362dd
Kimberly Marroquin <marroquin.kim3@gmail.com>
2022-07-02 19:32:03 -0400

    added audio file uploading functionality

--- bab-frontend/src/components/audio/UploadAudio.jsx
+++ bab-frontend/src/components/audio/UploadAudio.jsx
@@ -1,19 +1,15 @@
 import React, {useState} from 'react';
 import Form from 'react-bootstrap/Button';
 function UploadAudio() {
-    const [selectedFile, setSelectedFile] = useState();
-	const [isFilePicked, setIsFilePicked] = useState(false);
-    const [isSelected, setIsSelected] = useState(false);
 
 	return(
-
         <div>    
                  <Form.Group controlId="formFile" className="mb-3">
                     <Form.Label>Default file input example</Form.Label>
                     <Form.Control type="file" />
                 </Form.Group>
-             </div>
-         )
+         </div>
+    )
 }
 
 export default UploadAudio;
