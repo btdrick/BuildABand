b1cd03b65b93ef6dc6ab3add933b7c57c4d65053
Kimberly Marroquin <marroquin.kim3@gmail.com>
2022-07-02 15:39:05 -0400

    removed audio directory

--- bab-frontend/src/components/audio/UploadAudio.jsx
+++ bab-frontend/src/components/audio/UploadAudio.jsx
@@ -1,54 +1,17 @@
 import React, {useState} from 'react';
+import Form from 'react-bootstrap/Button';
 function UploadAudio() {
     const [selectedFile, setSelectedFile] = useState();
 	const [isFilePicked, setIsFilePicked] = useState(false);
     const [isSelected, setIsSelected] = useState(false);
 
-    const changeHandler = (event) => {
-		setSelectedFile(event.target.files[0]);
-		setIsSelected(true);
-	};
-
-
-	const handleSubmission = () => {
-		const formData = new FormData();
-		formData.append('File', selectedFile);
-		fetch(
-			'https://freeimage.host/api/1/upload?key=<YOUR_API_KEY>',
-			{
-				method: 'POST',
-				body: formData,
-			}
-		)
-			.then((response) => response.json())
-			.then((result) => {
-				console.log('Success:', result);
-			})
-			.catch((error) => {
-				console.error('Error:', error);
-			});
-	};
-
 	return(
 
         <div>    
-                 <input type="file" name="file" onChange={changeHandler} />    
-                 {isSelected ? (    
-                     <div>   
-                         <p>Filename: {selectedFile.name}</p>   
-                         <p>Filetype: {selectedFile.type}</p>   
-                         <p>Size in bytes: {selectedFile.size}</p>  
-                         <p>  
-                             lastModifiedDate:{' '}   
-                             {selectedFile.lastModifiedDate.toLocaleDateString()}   
-                         </p>    
-                     </div>    
-                 ) : (    
-                     <p>Select a file to show details</p>
-                 )}
-                 <div>
-                     <button onClick={handleSubmission}>Submit</button>
-                 </div>
+                 <Form.Group controlId="formFile" className="mb-3">
+                    <Form.Label>Default file input example</Form.Label>
+                    <Form.Control type="file" />
+                </Form.Group>
              </div>
          )
 }
