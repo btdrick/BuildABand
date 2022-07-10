8d4726958c2b0a1925558d31a722c03fa68cda35
Kimberly Marroquin <61890103+kmarroq1@users.noreply.github.com>
2022-07-10 11:39:07 -0400

    Revert "111 UI fixes"

--- bab-frontend/src/components/connection/AddConnection.js
+++ bab-frontend/src/components/connection/AddConnection.js
@@ -1,47 +1,37 @@
-import { React, useState, useEffect } from "react";
+import React, {Component} from "react";
 import { variables } from '../../Variables.js';
 import UserProfile from "../../components/UserProfile";
-import Button from 'react-bootstrap/Button'
 
-const AddConnection = ({ followerID, connection }) => {
-    const [isConnected, setIsConnected] = useState(false);
-    const [isPending, setIsPending] = useState(false);
+class AddConnection extends Component{
+    constructor(props){
+      super(props);  
 
-    /* Sets connection status */
-    useEffect(() => {
-        if (typeof connection === 'undefined') {
-            setIsConnected(false);
-            setIsPending(false);
-        }
-        if (typeof connection !== 'undefined') {
-            setIsConnected(connection.Connected === true);
-            setIsPending(connection.Connected === false);
-        }
-    }, [connection]);
-    
-    /* Send connection request to musician */
-    const sendConnectionRequest = async() => {
+      this.sendConnectionRequest = this.sendConnectionRequest.bind(this);
+    }
+
+    sendConnectionRequest(){
         fetch (variables.API_URL + "musicianconnections/" + 
-                UserProfile.getMusicianID()+"/" + followerID,{
+                UserProfile.getMusicianID()+"/" + this.props.followerID,{
                 method: "POST"})
                 .then(res => res.json())
                 .then(result=> alert(result))         
     }
 
-    return(
-        <div style={{display: 'flex', justifyContent: 'center', marginBottom: '1em'}}>
-            {!isConnected && !isPending &&
-            <Button className="btn btn-primary"
-                onClick={sendConnectionRequest}>
-                Add Connection
-            </Button>}
-            {isPending && !isConnected &&
-            <Button className="btn btn-secondary" disabled>
-                Connection Pending . . .
-            </Button>}
-            {isConnected && <p className="text-center text-muted">You're connected with this musician</p>}
-        </div>
-    )
+
+    render(){
+        return(
+            <div>
+                <button className="btn btn-primary"
+                    onClick={this.sendConnectionRequest}>
+                    Add Connection
+                </button>
+            </div>
+        )
+
+    }
+   
+
+    
 }
 
 export default AddConnection;
