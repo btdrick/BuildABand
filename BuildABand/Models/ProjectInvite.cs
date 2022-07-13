58830ec54b44e89a98995d069ee57a5cf9d1edde
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-07-13 05:41:56 +0100

    Add Project and ProjectInvite model class

---
+++ BuildABand/Models/ProjectInvite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Models
+{
+    /// <summary>
+    /// This class models a state object.
+    /// For status property 0 represent pending and 1 accept
+    /// </summary>
+    public class ProjectInvite
+    {
+        public int ProjectInviteID { get; set; }
+        public int ProjectID { get; set; }
+        public int InviterID { get; set; }
+        public string InviterNames { get; set; }
+        public int InviteeID { get; set; }
+        public string InviteeNames { get; set; }
+        public int Status { get; set; }
+
+    }
+}
