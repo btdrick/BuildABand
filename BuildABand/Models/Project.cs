5de3f2ebcf28830d50bc524d5008420c67c294a4
Amaefuna Chijioke <ca00077@my.westga.edu>
2022-07-13 05:41:56 +0100

    Add Project and ProjectInvite model class

---
+++ BuildABand/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildABand.Models
+{
+    public class Project
+    {
+        public int ProjectID { get; set; }
+        public string Name { get; set }
+        public int OwnerID { get; set }
+        public int OwnerNames { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<Musician> Collaborators { get; set; }
+        public List<int> Audios { get; set; }
+
+    }
+}
